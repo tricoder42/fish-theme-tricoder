a108c972aca48a42f4eb654ab6d3f32b5d9c4421
Tomáš Ehrlich <tomas.ehrlich@gmail.com>
2017-09-11 09:06:37 +0200

    Add nerd font glyphs

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -33,13 +33,6 @@ function fish_prompt -d "Tricoder's fish prompt"
     if set -l branch_local (git_branch_name)
         set -l branch_remote
         set -l branch_delimiter
-        set -l branch_status
-
-        if git_is_dirty
-            set branch_status "*"
-        else
-            set branch_status "+"
-        end
 
         if set -l git_upstream (__git_upstream)
             set branch_delimiter "$__glyph_branch"
@@ -53,23 +46,41 @@ function fish_prompt -d "Tricoder's fish prompt"
 
             set -l commit_count (__git_commit_count)
             set -l commit_ahead (string trim $commit_count[1])
+            set -l commit_behind (string trim $commit_count[3])
+            
+            set -l git_action
+            if test $commit_ahead -gt 0 -a $commit_behind -gt 0
+                # need merge
+                set git_action '\uf126'
+            else if test $commit_ahead -gt 0
+                # need push
+                set git_action '\uf0ee'
+            else if test $commit_behind -gt 0
+                # need fast forward
+                set git_action '\uf102'
+            else
+                # all good!
+                set git_action '\uf109'
+            end
+
+
             if test $commit_ahead -gt 0
               set commit_ahead "+$commit_ahead"
             else
               set commit_ahead "--"
             end
-            set -l commit_behind (string trim $commit_count[3])
+
             if test $commit_behind -gt 0
               set commit_behind "-$commit_behind"
             else
               set commit_behind "--"
             end
 
-            __print_color $fish_color_git " $commit_behind | $commit_ahead"
+            __print_color $fish_color_git " $commit_behind $git_action $commit_ahead"
         end
 
         __print_color $fish_color_git " ($branch_local$branch_delimiter$branch_remote)"
     end
 
-    printf "\e[K\n❯ "
+    printf "\e[K\n>>= "
 end
