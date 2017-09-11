9dbb388703bebdba2a3ecd7890b4eb38d9c1d378
Tomáš Ehrlich <tomas.ehrlich@gmail.com>
2017-09-11 10:52:15 +0200

    Add date and last command status

--- functions/fish_right_prompt.fish
+++ functions/fish_right_prompt.fish
@@ -1,2 +1,11 @@
 function fish_right_prompt
+  set -l last_command_status $status
+
+  if test $last_command_status != 0
+    set_color red
+    printf "$last_command_status ↵ "
+    set_color normal
+  end
+
+  printf (date "+%H:%M:%S")
 end
