19a5d357b763fa34d37d01c3ba19411b5a594c2e
Tomáš Ehrlich <tomas.ehrlich@gmail.com>
2017-09-11 08:52:38 +0200

    Show commit count compared to remote branch

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -15,16 +15,24 @@ function __print_bold
     set_color normal
 end
 
-function __git_upstream_configured
-    git rev-parse --abbrev-ref @"{u}" > /dev/null 2>&1
+function __git_upstream
+    string split "/" -- (git rev-parse --abbrev-ref @"{u}" 2>&1)
+end
+
+function __git_commit_count
+    string split "" (git rev-list --left-right --count HEAD...@"{u}" 2>&1)
 end
 
 function fish_prompt -d "Tricoder's fish prompt"
+    set -l __glyph_branch " ᄉ"
+
     echo -e ""
     set -l pwd_path (prompt_pwd)
     __print_color $fish_color_cwd "$pwd_path"
 
-    if set -l branch_name (git_branch_name)
+    if set -l branch_local (git_branch_name)
+        set -l branch_remote
+        set -l branch_delimiter
         set -l branch_status
 
         if git_is_dirty
@@ -33,20 +41,34 @@ function fish_prompt -d "Tricoder's fish prompt"
             set branch_status "+"
         end
 
-        echo -sn (set_color -o) " ($branch_name$branch_status) " (set_color normal)
+        if set -l git_upstream (__git_upstream)
+            set branch_delimiter "$__glyph_branch"
 
-        if __git_upstream_configured
-             set -l git_ahead (command git rev-list --left-right --count HEAD...@"{u}" ^ /dev/null | awk '
-                $1 > 0 { printf("⇡") } # can push
-                $2 > 0 { printf("⇣") } # can pull
-             ')
-
-             if test ! -z "$git_ahead"
-                __print_color $fish_color_git " $git_ahead"
+            if [ "$git_upstream[2]" = $branch_local ]
+                set branch_remote $git_upstream[1]
+            else
+                set branch_remote (string join "/" $git_upstream)
             end
-        else
-          printf "Upstream not configured"
+
+
+            set -l commit_count (__git_commit_count)
+            set -l commit_ahead (string trim $commit_count[1])
+            if test $commit_ahead -gt 0
+              set commit_ahead "+$commit_ahead"
+            else
+              set commit_ahead "--"
+            end
+            set -l commit_behind (string trim $commit_count[3])
+            if test $commit_behind -gt 0
+              set commit_behind "-$commit_behind"
+            else
+              set commit_behind "--"
+            end
+
+            __print_color $fish_color_git " $commit_behind | $commit_ahead"
         end
+
+        __print_color $fish_color_git " ($branch_local$branch_delimiter$branch_remote)"
     end
 
     printf "\e[K\n❯ "
